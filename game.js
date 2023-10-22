4307a1bcbf740d5e8c152c7e927d3fbc92aeb13e
Sairaj Pawar <sairajepawar994@gmail.com>
2023-10-22 17:38:47 +0530

    fixed some glitches which occured while starting another one and also add a button to start the game

--- game.js
+++ game.js
@@ -3,12 +3,30 @@ var colorAvaliable = ["green","red","yellow","blue"]
 var beginning  = true;
 var noOfLevel = 1;
 var pre;
+
+// play sound of respective button when pressed
 function soundOnClick(temp)
 {
     var path = "sounds/"+temp+".mp3";
     var audio = new Audio(path);
     audio.play();
 }
+
+function newSession(){
+    // activate function when game is newly opened or previous game is over
+    if(beginning){
+        $(".btn-start").hide();
+        beginning=false;
+        pre = new Date();
+        $(".result").hide();
+        $(".time_taken").hide();
+        // reset the sequence array
+        colorSequence.splice(0,colorSequence.length);
+        newLevel();
+    }
+}
+
+// all operation required to continue to nextlevel
 function newLevel(){
     //selecting random color
     var index = Math.floor(Math.random()*4);
@@ -26,6 +44,7 @@ function newLevel(){
     // console.log(levelTitle);
     $("#level-title").text(levelTitle);
 }
+// animate the press even
 function animatePress(temp){
     var target = "#"+temp;
     console.log(target);
@@ -34,19 +53,22 @@ function animatePress(temp){
         $(target).removeClass("pressed");
     },100);
 }
-//keypress to start game only 
+
+// start button press to start game
+$(".btn-start").click(()=>{
+    newSession();
+})
+
+//keypress to start game 
 addEventListener("keypress",function(){
-    if(beginning){
-        newLevel();
-        beginning=false;
-        pre = new Date();
-    }
+    newSession();
 });
-var it = 0 ; //it will be used to iterate through colorSequnce as we keep clicking buttons
-// for comparing the sequence entered by user with the answer
+
+// iterator to check the sequence entered
+var it = 0 ;
 $(".btn").click(function(){
     var temp = $(this).attr("id");
-    animatePress(temp);// issue here
+    animatePress(temp);
     if(colorSequence[it]==temp)
     {
         it++;
@@ -54,25 +76,26 @@ $(".btn").click(function(){
     }
     else
     {
-        //game over condition i.e input is not according to sequence
+        //sequence not matched wrong answer
+        // change title
         $("#level-title").text("Game Over, Press Any Key to Restart");
-        soundOnClick("wrong");
+        soundOnClick("wrong"); //play the sound for failed level
+        // animate the fail case
         $("body").addClass("game-over");
         setTimeout(function(){
             $("body").removeClass("game-over");  
         },100)
-        // to display the result in terms of time taken and levels completed
-        var statement1 = `Level Reached ${noOfLevel}`;
-        // calculate time taken till now
+        var statement1 = `Level Reached ${noOfLevel-1}`;
         var cur = new Date();
-        var time = (cur-pre)/1000; //as inbuilt function use milisecond as unit for time
+        var time = (cur-pre)/1000; //as Date case calculates time in milliseconds by default
         var statement2 = `Time Taken :- ${time} seconds`;
+        $(".result").show();
+        $(".time_taken").show();
         $(".result").text(statement1);
         $(".time_taken").text(statement2);
-        //reset the number of levels
         noOfLevel = 1;
-        //reset the beginning condition
         beginning=true;
+        $(".btn-start").show();
     }
     if(it==colorSequence.length)
     {
